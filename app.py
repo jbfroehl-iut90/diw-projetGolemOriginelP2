0032396b34e71aa1cafbb3078b475651afca715a
jb_frhl <146169903+jbfroehl-iut90@users.noreply.github.com>
2023-12-27 15:12:53 +0100

    cérelouuuuu

--- app.py
+++ app.py
@@ -57,13 +57,12 @@ def add_marque():
 
     sql='''
     SELECT motos.id_moto AS id,
-    motos.nom_moto AS nom,
+    motos.libelle_moto AS nom,
     motos.puissance_moto AS puissance,
     motos.couleur_moto AS couleur,
-    motos.miseEnCirculation_moto AS miseEnCirculation,
-    motos.photo_moto AS photo,
-    marques.libelle_marque AS marque
-    FROM motos INNER JOIN marques ON motos.marque_id_marque = marques.id_marque
+    motos.date_mise_en_circulation AS miseEnCirculation,
+    motos.photo_moto AS photo
+    FROM motos INNER JOIN marques ON motos.marque_id = marques.id_marque
     '''
 
     mycursor.execute(sql)
@@ -80,7 +79,7 @@ def valid_add_marque():
     tuple_insert = (libelle, logo)
     sql = '''
     INSERT INTO marques (libelle_marque, logo_marque)
-    VALUES (%s, %s));
+    VALUES (%s, %s)
     '''
     mycursor.execute(sql, tuple_insert)
     get_db().commit()
@@ -91,79 +90,217 @@ def valid_add_marque():
 
 @app.route('/marque/delete', methods=['GET'])
 def delete_marque():
-    id = request.args.get('id', '')
-    print ("une marque supprimée, id : ",id)
-    message=u'une marque supprimée, id : ' + id
+    mycursor = get_db().cursor()
+    id_marque = request.args.get('id', '')
+    tuple_delete = (id_marque)
+    sql = '''
+    DELETE FROM marques
+    WHERE id_marque = %s
+    '''
+    mycursor.execute(sql, tuple_delete)
+    get_db().commit()
+    message=u'Une marque supprimée ! id : ' + id_marque
     flash(message, 'alert-warning')
-    return redirect('/marque/show')
+    return redirect('/ticket/show')
 
 
 @app.route('/marque/edit', methods=['GET'])
 def edit_marque():
-    id = request.args.get('id', '')
-    id=int(id)
-    marque = marques[id-1]
+    mycursor = get_db().cursor()
+    id_marque = request.args.get('id', '')
+    tuple_edit = (id_marque)
+    sql = '''
+    SELECT marques.id_marque AS id,
+    marques.libelle_marque AS nom,
+    marques.logo_marque AS logo
+    FROM marques 
+    WHERE id_marque = %s
+    '''
+    mycursor.execute(sql, tuple_edit)
+    marque = mycursor.fetchone()
     return render_template('brand/edit_marque.html', marque=marque)
 
 @app.route('/marque/edit', methods=['POST'])
 def valid_edit_marque():
-    id = request.form.get('id', '')
-    libelle = request.form.get('libelle', '')
-    logo = request.form.get('logo', '')
+    mycursor = get_db().cursor()
+    id_marque = request.form['id']
+    libelle = request.form['libelle']
+    logo = request.form['logo']
+    tuple_edit = (libelle, logo, id_marque)
+    sql = '''
+    UPDATE marques
+    SET libelle_marque = %s, logo_marque = %s
+    WHERE id_marque = %s
+    '''
+    mycursor.execute(sql, tuple_edit)
+    get_db().commit()
     print(u'marque modifiée , libelle : ', libelle, ' | logo : ', logo)
-    message = u'marque modifiée , libelle:'+libelle + ' | logo : ' + logo
+    message = u'marque modifiée , libelle : '+libelle + ' | logo : ' + logo
     flash(message, 'alert-success')
     return redirect('/marque/show')
 
 @app.route('/moto/show')
 def show_moto():
-    return render_template('moto/show_moto.html', moto=motos, brand=marques)
+    mycursor = get_db().cursor()
+    sql = '''
+    SELECT 
+        motos.id_moto AS id,
+        motos.libelle_moto AS nom,
+        motos.puissance_moto AS puissance,
+        motos.couleur_moto AS couleur,
+        motos.date_mise_en_circulation AS miseEnCirculation,
+        motos.photo_moto AS photo,
+        marques.libelle_marque AS marque,
+        marques.logo_marque AS logo
+    FROM motos INNER JOIN marques ON motos.marque_id = marques.id_marque
+    '''
+    mycursor.execute(sql)
+    motos= mycursor.fetchall()
+
+    return render_template('moto/show_moto.html', moto=motos)
 
 @app.route('/moto/add', methods=['GET'])
 def add_moto():
+    mycursor = get_db().cursor()
+    sql='''
+    SELECT marques.id_marque AS id,
+    marques.libelle_marque AS nom,
+    marques.logo_marque AS logo
+    FROM marques 
+    '''
+
+    mycursor.execute(sql)
+    marques= mycursor.fetchall()
+
+    sql='''
+    SELECT motos.id_moto AS id,
+    motos.libelle_moto AS nom,
+    motos.puissance_moto AS puissance,
+    motos.couleur_moto AS couleur,
+    motos.date_mise_en_circulation AS miseEnCirculation,
+    motos.photo_moto AS photo
+    FROM motos INNER JOIN marques ON motos.marque_id = marques.id_marque
+    '''
+
+    mycursor.execute(sql)
+    motos= mycursor.fetchall()
+
     return render_template('moto/add_moto.html', brand=marques, moto=motos)
 
 @app.route('/moto/add', methods=['POST'])
 def valid_add_moto():
-    nom = request.form.get('nom', '')
-    brand_id = request.form.get('marque_id', '')
-    puissance = request.form.get('puissance', '')
-    couleur = request.form.get('couleur', '')
-    miseEnCirculation = request.form.get('miseEnCirculation', '')
-    photo = request.form.get('photo', '')
-    print(u'Nouvelle moto , nom : ', nom, ' | id marque : ', brand_id, ' | Puissance : ', puissance, ' | couleur : ', couleur, ' | Mise en circulation : ', miseEnCirculation, ' | photo : ', photo)
-    message = u'Nouvelle moto , nom : '+nom + ' | id marque : ' + brand_id + ' | Puissance : ' + puissance + ' | couleur : '+  couleur + ' | Mise en circulation : ' + miseEnCirculation + ' | photo : ' + photo
+    mycursor = get_db().cursor()
+
+    libelle = request.form['nom']
+    marque_id = request.form['marque_id']
+    puissance = request.form['puissance']
+    miseEnCirculation = request.form['miseEnCirculation']
+    couleur = request.form['couleur']
+    photo = request.form['photo']
+    tuple_insert = (libelle, marque_id, puissance, miseEnCirculation, couleur, photo)
+    sql = '''
+    INSERT INTO motos (libelle_moto, marque_id, puissance_moto, date_mise_en_circulation, couleur_moto, photo_moto)
+    VALUES (%s, %s, %s, %s, %s, %s)
+    '''
+    mycursor.execute(sql, tuple_insert)
+    get_db().commit()
+    print(u'Nouvelle moto , libelle : ', libelle, ' | marque_id : ', marque_id, ' | puissance : ', puissance, ' | mise en circulation : ', miseEnCirculation, ' | couleur : ', couleur, ' | photo : ', photo)
+    message = u'Nouvelle moto , libelle : '+libelle + ' | marque_id : ' + marque_id + ' | puissance : ' + puissance + ' | mise en circulation : ' + miseEnCirculation + ' | couleur : ' + couleur + ' | photo : ' + photo
     flash(message, 'alert-success')
     return redirect('/moto/show')
 
 @app.route('/moto/delete', methods=['GET'])
 def delete_moto():
-    id = request.args.get('id', '')
-    print ("une moto supprimée, id : ",id)
-    message=u'un article supprimé, id : ' + id
+    mycursor = get_db().cursor()
+    id_moto = request.args.get('id', '')
+    tuple_delete = (id_moto)
+    sql = '''
+    DELETE FROM motos
+    WHERE id_moto = %s
+    '''
+    mycursor.execute(sql, tuple_delete)
+    get_db().commit()
+    message=u'Une moto supprimée ! id : ' + id_moto
     flash(message, 'alert-warning')
-    return redirect('/moto/show')
+    return redirect('/ticket/show')
 
 @app.route('/moto/edit', methods=['GET'])
 def edit_moto():
-    id = request.args.get('id', '')
-    id=int(id)
-    moto1 = motos[id-1]
-    return render_template('moto/edit_moto.html', moto=moto1, brand=marques)
+    mycursor = get_db().cursor()
+    id_moto = request.args.get('id', '')
+    tuple_edit = (id_moto)
+    sql = '''
+    SELECT motos.id_moto AS id,
+    motos.libelle_moto AS nom,
+    motos.puissance_moto AS puissance,
+    motos.couleur_moto AS couleur,
+    motos.date_mise_en_circulation AS miseEnCirculation,
+    motos.photo_moto AS photo,
+    marques.libelle_marque AS marque
+    FROM motos INNER JOIN marques ON motos.marque_id = marques.id_marque
+    WHERE id_moto = %s
+    '''
+    mycursor.execute(sql, tuple_edit)
+    moto = mycursor.fetchone()
+    sql='''
+    SELECT marques.id_marque AS id,
+    marques.libelle_marque AS nom,
+    marques.logo_marque AS logo
+    FROM marques
+    '''
+    mycursor.execute(sql)
+    marques= mycursor.fetchall()
+
+    # Récupère les couleurs
+    sql='''
+    SELECT DISTINCT couleur_moto AS couleur
+    FROM motos
+    '''
+    mycursor.execute(sql)
+    couleurs= mycursor.fetchall()
+
+
+    return render_template('moto/edit_moto.html', moto=moto, brand=marques, couleur=couleurs)
 
 @app.route('/moto/edit', methods=['POST'])
+# Requête préparée avec SET PREPARE et EXECUTE pour valid_edit_moto
 def valid_edit_moto():
-    id = request.form.get('id', '')
-    nom = request.form.get('nom', '')
-    brand_id = request.form.get('marque_id')
-    puissance = request.form.get('puissance', '')
-    miseEnCirculation = request.form.get('miseEnCirculation', '')
-    couleur = request.form.get('couleur', '')
-    photo = request.form.get('photo', '')
-    print(u'moto modifiée , nom : ', nom, ' | brand_id :', brand_id, ' | puissance:', puissance, ' | mise en circulation:', miseEnCirculation, ' | couleur :', couleur, ' | image:', photo)
-    message = u'moto modifiée , nom:'+nom + ' | brand_id :' + str(brand_id) + ' | puissance:' + puissance + ' | mise en circulation:'+  miseEnCirculation + ' | couleur:' + couleur + ' | image:' + photo
+    mycursor = get_db().cursor()
+    id_moto = request.form['id']
+    libelle = request.form['nom']
+    marque_id = request.form['marque_id']
+    puissance = request.form['puissance']
+    miseEnCirculation = request.form['miseEnCirculation']
+    couleur = request.form['couleur']
+    photo = request.form['photo']
+    tuple_edit = (libelle, int(id_moto), int(puissance), miseEnCirculation, couleur, photo, int(marque_id))
+
+    sql = '''
+    SET @nouveau_nom = 'test';
+    SET @id_moto = '25';
+    SET @nouvelle_puissance = '3000';
+    SET @nouvelle_mise_en_circulation = '2023-12-06';
+    SET @nouvelle_couleur = 'bleu';
+    SET @nouvelle_photo = 'hdsgih';
+    SET @marque_id = 3;
+
+    PREPARE stmt FROM 'UPDATE motos SET libelle_moto = ?, puissance_moto = ?, date_mise_en_circulation = ?, couleur_moto = ?, marque_id = ?, photo_moto = ? WHERE id_moto = ?';
+
+    EXECUTE stmt USING @nouveau_nom, @nouvelle_puissance, @nouvelle_mise_en_circulation, @nouvelle_couleur, @marque_id, @nouvelle_photo, @id_moto;
+
+    DEALLOCATE PREPARE stmt;
+    '''
+
+    mycursor.execute(sql)
+    get_db().commit()
+
+    message = f"Moto modifiée - Libelle: {libelle}, Marque_id: {marque_id}, Puissance: {puissance}, Mise en circulation: {miseEnCirculation}, Couleur: {couleur}, Photo: {photo}"
+    
     flash(message, 'alert-success')
+    
     return redirect('/moto/show')
+
+
 
 @app.route('/moto/filtre', methods=['GET'])
 def filtre_moto():
